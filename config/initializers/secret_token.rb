dd4998c75b0a53f88fc1e429610e24fb2481f872
K2 <k2@ubuntu.ubuntu-domain>
2014-02-27 11:07:06 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bed::Application.config.secret_key_base = 'a7c8b135f98280d3257c324cd7fc29a0294f8eec83cff5a3dc53e44b7e8bfcf62975f9450ce1c41a1bf4851e4eb1827c4e529c96fb735eff4c1a0a64f93272d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bed::Application.config.session_store :cookie_store, key: '_bed_session'
